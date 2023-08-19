e47b0db407d53792e6d34e08544c968fd7d97fc6
brianaderer <brian@brianaderer.com>
2023-08-19 14:29:13 -0400

    begin installing nextauth.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import App from '../app/_app'
-
 const Index = (props: any) => {
     console.log(props)
     return (
         <div>
-            {props.user ? (
-                <>
-                    <span>Signed in as : {props.user.email}</span>
-                    <button onClick={props.signOut}>Sign Out</button>
-                </>
-            ) : (
-                <button onClick={props.signIn}>Sign In</button>
-            )}
+
+            <h1>hello world</h1>
+            {/*{props.user ? (*/}
+            {/*    <>*/}
+            {/*        <span>Signed in as : {props.user.email}</span>*/}
+            {/*        <button onClick={props.signOut}>Sign Out</button>*/}
+            {/*    </>*/}
+            {/*) : (*/}
+            {/*    <button onClick={props.signIn}>Sign In</button>*/}
+            {/*)}*/}
         </div>
     )
 }
