847e2654b64cb703f003f8ad3efe6c499e5d9376
brianaderer <brian@brianaderer.com>
2023-08-19 23:36:12 -0400

    google working on firebase login, working on getting email login to work also

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,12 +2,18 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
-
+import { cert } from "firebase-admin/app";
 
 // @ts-ignore
 export default NextAuth({
     //@ts-ignore
-    // adapter: FirestoreAdapter(),
+    adapter: FirestoreAdapter({
+        credential: cert({
+            projectId: process.env.projectId,
+            clientEmail: process.env.client_email,
+            privateKey: process.env.private_key,
+        }),
+    }),
     providers: [
         GoogleProvider({
             // @ts-ignore
@@ -15,10 +21,10 @@ export default NextAuth({
             // @ts-ignore
             clientSecret: process.env.googleClientSecret
         }),
-        // EmailProvider({
-        //     server: process.env.EMAIL_SERVER,
-        //     from: process.env.EMAIL_FROM
-        // }),
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM
+        }),
     ],
     callbacks: {
         session({ session, token, user }) {
