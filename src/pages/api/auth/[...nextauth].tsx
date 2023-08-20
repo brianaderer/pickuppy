feb876d0bf6a8a2b9b52c7a03d861ac39d6584ed
brianaderer <brian@brianaderer.com>
2023-08-19 21:10:37 -0400

    google auth stable

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 
 
+// @ts-ignore
 export default NextAuth({
+    //@ts-ignore
+    // adapter: FirestoreAdapter(),
     providers: [
         GoogleProvider({
             // @ts-ignore
@@ -11,12 +15,10 @@ export default NextAuth({
             // @ts-ignore
             clientSecret: process.env.googleClientSecret
         }),
-        EmailProvider({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM
-        }),
-
-
+        // EmailProvider({
+        //     server: process.env.EMAIL_SERVER,
+        //     from: process.env.EMAIL_FROM
+        // }),
     ],
     callbacks: {
         session({ session, token, user }) {
